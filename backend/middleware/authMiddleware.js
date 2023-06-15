11e81bceb53ff274d61f75a7343733724ec31e09
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2023-06-15 18:46:42 +0100

    authentication middleware updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,6 +28,33 @@ const protect = asyncHandler(async (req, res, next) => {
 	}
 })
 
+const phoneNumberProtect = asyncHandler(async (req, res, next) => {
+	let token
+
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		try {
+			token = req.headers.authorization.split(' ')[1]
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+			req.user = await User.findOne({phoneNumber: decoded.phoneNumber})
+
+			next()
+		} catch (error) {
+			console.error(error)
+			res.status(401)
+			throw new Error('Not authorized, token failed')
+		}
+	}
+	if (!token) {
+		res.status(401)
+		throw new Error('Not authorized, no token')
+	}
+
+})
+
 const admin = (req, res, next) => {
 	if (req.user && req.user.isAdmin) {
 		next()
@@ -37,4 +64,4 @@ const admin = (req, res, next) => {
 	}
 }
 
-export { protect, admin }
+export { protect, admin, phoneNumberProtect }
